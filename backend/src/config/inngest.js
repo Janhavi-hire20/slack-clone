e5eabd6b605ec06811bb959598ea76880d706924
Janhavi-hire20 <hirejanhavi1@gmail.com>
2025-11-13 14:59:01 +0530

    tiny change in inngest.js

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -18,7 +18,7 @@ const syncUser = inngest.createFunction(
 
         const newUser = {
             clerkId: id,
-            email: email_addresses[0]?.email || "",
+            email: email_addresses[0]?.email_address || "",
             name: `${first_name || ""} ${last_name || ""}`,
             image: image_url,
         };
