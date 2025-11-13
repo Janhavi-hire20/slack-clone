0e42541b129865d2330d68c6fc334f30512d72f1
Janhavi-hire20 <hirejanhavi1@gmail.com>
2025-11-13 11:37:34 +0530

    tiny fixes for inngest.js file

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,8 +1,8 @@
 import { Inngest } from "inngest";
 import { connect } from "mongoose";
 import { connectDB } from "./db";
-import { User } from "@clerk/express";
-
+//import { User } from "@clerk/express";
+import { User } from "../models/User.js";
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "slack-clone" });
 
@@ -31,6 +31,7 @@ const deleteUserFromDB = inngest.createFunction(
     { id: "delete-user-from-db" },
     { event: "clerk/user.deleted" },
     async ({ event }) => {
+        await connectDB();
         const { id } = event.data;
         await User.deleteOne({ clerkId: id });
     }
