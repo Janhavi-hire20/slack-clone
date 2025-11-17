7a82af2a7a63e1a45010a10791938932d72926fe
Janhavi-hire20 <hirejanhavi1@gmail.com>
2025-11-17 11:38:13 +0530

    stream and sentry added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,13 @@
+import "../instrument.mjs";//sentry instrumentation must be the first import
 import express from 'express';
 import { ENV } from './config/env.js';
 import { connectDB } from './config/db.js';
 import { clerkMiddleware } from '@clerk/express';
 import {functions, inngest} from './config/inngest.js';
 import { serve } from "inngest/express";
+import chatRoutes from './routes/chat.route.js';
+
+import * as Sentry from "@sentry/node";
 
 const app = express();
 app.use(express.json());//access to req.body
@@ -12,18 +16,27 @@ app.use(express.json());//access to req.body
 
 app.use(clerkMiddleware());//req.auth will be available in the request object
 
-app.use("/api/inngest", serve({ client: inngest, functions }));
+app.get("/debug-sentry", (req, res) => {
+    throw new Error("Sentry debug error!");
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/chat",chatRoutes);
+
+Sentry.setupExpressErrorHandler(app);
+
 const startServer = async () => {
     try {
         await connectDB(); 
+        app.listen(ENV.PORT ,() =>{
         if (ENV.NODE_ENV === 'development') {
             console.log('Connected to the port:', ENV.PORT );
-        }     
+        }  
+    }); 
     }
     catch (error) {
         console.error('Failed to connect to the server', error);
