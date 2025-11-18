b98a80226adc92d95f0cb3632e4143810a6bec5a
Janhavi-hire20 <hirejanhavi1@gmail.com>
2025-11-18 19:57:28 +0530

    channel setup done successfully

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,15 +3,15 @@ import express from 'express';
 import { ENV } from './config/env.js';
 import { connectDB } from './config/db.js';
 import { clerkMiddleware } from '@clerk/express';
-import {functions, inngest} from './config/inngest.js';
+import { functions, inngest } from './config/inngest.js';
 import { serve } from "inngest/express";
 import chatRoutes from './routes/chat.route.js';
-
 import * as Sentry from "@sentry/node";
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());//access to req.body
-
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));//enable CORS for all routes
 
 
 app.use(clerkMiddleware());//req.auth will be available in the request object
@@ -25,18 +25,18 @@ app.get('/', (req, res) => {
 });
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
-app.use("/api/chat",chatRoutes);
+app.use("/api/chat", chatRoutes);
 
 Sentry.setupExpressErrorHandler(app);
 
 const startServer = async () => {
     try {
-        await connectDB(); 
-        app.listen(ENV.PORT ,() =>{
-        if (ENV.NODE_ENV === 'development') {
-            console.log('Connected to the port:', ENV.PORT );
-        }  
-    }); 
+        await connectDB();
+        app.listen(ENV.PORT, () => {
+            if (ENV.NODE_ENV === 'development') {
+                console.log('Connected to the port:', ENV.PORT);
+            }
+        });
     }
     catch (error) {
         console.error('Failed to connect to the server', error);
